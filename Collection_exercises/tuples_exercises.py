c0b37488d7bc76bd372d145f05558233437c2058
Omaima <omaimamutsem@gmail.com>
2026-09-12 12:51:50 +0400

    tuples exercises

---
+++ Collection_exercises/tuples_exercises.py
@@ -0,0 +1,8 @@
+# Tuples exercises:
+my_tuple = (1, 2, 3, 4, 5)
+print(type(my_tuple))
+
+print(len(my_tuple))  # the length of the tuple
+print(my_tuple[0])  # access the first element of the tuple
+print(my_tuple[-1])  # access the last element of the tuple
+print(my_tuple[1:3])  # access a slice of the tuple
