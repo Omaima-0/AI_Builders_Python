65a55f483417dced2cc74e9c46a9c84e5490fbf5
Omaima <omaimamutsem@gmail.com>
2026-09-13 09:44:53 +0400

    while loop exercises

---
+++ loops/while_loop.py
@@ -0,0 +1,19 @@
+# While Loop
+
+# while condtion is True:
+#     # Code to be executed
+
+# i = 1
+# while i < 6:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+
+y = 0
+while y < 6:
+    y += 1
+    if y == 3:
+        continue  # skip the rest of the code inside the loop for this iteration
+
+    print(y)
