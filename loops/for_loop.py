36aefa6e9f2a7174c022db094915c6f98115391f
Omaima <omaimamutsem@gmail.com>
2026-09-13 09:59:52 +0400

    for-loop exercises

---
+++ loops/for_loop.py
@@ -0,0 +1,24 @@
+# for loop:
+# fruits = ["apple", "cucamber", "banana", "cherry"]
+
+# for fruit in fruits:
+#     if fruit == "cucamber":
+#         continue  # skip the rest of the code inside the loop for this iteration
+#     else:
+#         print(fruit)
+
+# for i in "apple":
+#     print(i)
+
+# for i in range(5):
+#     print(i)
+
+#####################################
+# nested loop
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for i in adj:
+    for j in fruits:
+        print(i, j)
